9b84b3f92e374e61aea2142d11e0b63c257951ad
SangNguyen1407 <14.sangnguyen.07@gmail.com>
2025-06-15 17:49:28 +0900

    google driver sheet

--- include/wifiHandle.h
+++ include/wifiHandle.h
@@ -8,11 +8,11 @@
 
 class WIFI_NETWORK{
     private:
-        const char* ssid = "00CAE0892657-2G";// Wi-FiのSSID
-        const char* password = "segmgx6nxz7c98";// Wi-Fiのパスワード
+        const char* ssid = "";// Wi-FiのSSID
+        const char* password = "";// Wi-Fiのパスワード
 
         const char* host = "script.google.com";
-        String url = "https://script.google.com/macros/s/AKfycbzvH3nMD7we4A6SaylcxTOV6WDbDiaDXJYYRdLYkrZJ4t7oX9vzHp8sFG150SSLQ6T_sw/exec";
+        String url = "";
         String hostName;
         String dnsIP;
         String macAddress;
